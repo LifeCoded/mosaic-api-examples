f29acd3d037f662ce9832e129b98eb4ffc529225
Yu-Hsuan <demiwu96@gmail.com>
2023-05-12 10:29:36 -0400

    add standard role examples

---
+++ src/standard_roles/deletes_a_standard_role.tsx
@@ -0,0 +1,13 @@
+import api from '../api';
+
+async function deleteStandardRole(teamId: number, roleId: number): Promise<void> {
+  try {
+    const response = await api.delete(`/api/${teamId}/rate/${roleId}`);
+    console.log(response.status);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Example usage for deleting a standard role with ID 67890
+deleteStandardRole(12345, 67890);
---
+++ src/standard_roles/fetch_all_standard_roles.tsx
@@ -0,0 +1,14 @@
+import api from '../api';
+
+async function fetchAllStandardRoles(teamId: number): Promise<void> {
+  try {
+    const response = await api.get(`/api/${teamId}/role`);
+    const standardRoles = response.data.roles;
+    console.log(standardRoles)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Example usage for fetching all standard roles
+fetchAllStandardRoles(12345);
