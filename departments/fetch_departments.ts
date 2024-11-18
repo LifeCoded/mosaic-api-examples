b578151fcc887754eda0d2d6034ff13273022a03
darshan487 <darshan.vanol@mosaicapp.com>
2024-11-18 15:50:47 +0530

    Updated departments apis

--- departments/fetch_departments.ts
+++ departments/fetch_departments.ts
@@ -3,7 +3,7 @@ import api from '../api';
 async function fetchDepartments(teamId: number): Promise<void> {
   try {
     const response = await api.get(`/api/${teamId}/department`);
-    const departments = response.data.departments;
+    const departments = (response.data as { departments: any[] }).departments;
     const totalCount = departments?.length ?? 0;
     console.log(departments);
     console.log(totalCount);
