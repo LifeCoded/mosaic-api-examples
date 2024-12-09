05e2d8dd939225d63034ef7b6c1782360892309c
darshan487 <darshan.vanol@mosaicapp.com>
2024-12-09 14:56:46 +0530

    Minor fix

--- work_categories/fetch_all_work_categories.ts
+++ work_categories/fetch_all_work_categories.ts
@@ -3,8 +3,8 @@ import api from '../api';
 async function fetchAllWorkCategories(teamId: number): Promise<void> {
   try {
     const response = await api.get(`/api/${teamId}/standard_work_category`);
-    const data = response.data as { standard_work_category: any[] };
-    const workCategories = data.standard_work_category;
+    const data = response.data as { standard_work_categories: any[] };
+    const workCategories = data.standard_work_categories;
     const totalCount = workCategories && workCategories.length ? workCategories.length : 0;
     console.log(workCategories);
     console.log(totalCount);
